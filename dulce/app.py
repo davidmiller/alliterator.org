2fb500687883c945d639479f13d4b8c0d379bfbd
David Miller <david@deadpansincerity.com>
2014-05-22 14:17:47 +0100

    Results side-by-side Add POS tagging API & section Add nltk data. Make text black on white

--- dulce/app.py
+++ dulce/app.py
@@ -1,13 +1,42 @@
 """
 Main Flask application object
 """
+import collections
+import functools
+import json
+
 from flask import (Flask, abort, request, redirect, Response, make_response,
                    session, url_for)
-
 from flask import render_template
+import nltk
+nltk.data.path.append('./nltk_data/')
+
 app = Flask(__name__)
 app.debug = True
+
+def jsonp(fn):
+    @functools.wraps(fn)
+    def with_callback_maybe(*args,**kwargs):
+        results = fn(*args,**kwargs)
+        results = json.dumps(results)
+        if  request.args.get('callback', None):
+            return '{0}({1})'.format(request.args.get('callback'), results)
+        else:
+            return Response(results, mimetype='application/json')
+    return with_callback_maybe
+
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/api/1/pos', methods=['POST'])
+@jsonp
+def pos_tag_post():
+    corpus = nltk.word_tokenize(request.json['corpus'].lower())
+    tagged = nltk.pos_tag(corpus)
+    grouped = collections.defaultdict(list)
+    for word, pos in tagged:
+        if word not in grouped[pos]:
+            grouped[pos].append(word)
+    return grouped
--- dulce/static/grayscale/js/app.js
+++ dulce/static/grayscale/js/app.js
@@ -1,3 +1,43 @@
+POSTAGS =  {
+
+    CC: 'COORDINATING CONJUNCTION',
+    CD: 'CARDINAL NUMBER',
+    DT: 'DETERMINER',
+    EX: 'EXISTENTIAL THERE',
+    FW: 'FOREIGN WORD',
+        IN: 'PREPOSITION OR SUBORDINATING CONJUNCTION',
+    JJ: 'ADJECTIVE',
+    JJR: 'ADJECTIVE, COMPARATIVE',
+    JJS: 'ADJECTIVE, SUPERLATIVE',
+    LS: 'LIST ITEM MARKER',
+    MD: 'MODAL',
+    NN: 'NOUN, SINGULAR OR MASS',
+    NNS: 'NOUN, PLURAL',
+    NNP: 'PROPER NOUN, SINGULAR',
+    NNPS: 'PROPER NOUN, PLURAL',
+    PDT: 'PREDETERMINER',
+    POS: 'POSSESSIVE ENDING',
+    PRP: 'PERSONAL PRONOUN',
+    PRP$: 'POSSESSIVE PRONOUN',
+    RB: 'ADVERB',
+    RBR: 'ADVERB, COMPARATIVE',
+    RBS: 'ADVERB, SUPERLATIVE',
+    RP: 'PARTICLE',
+    SYM: 'SYMBOL',
+    TO: 'TO',
+    UH: 'INTERJECTION',
+    VB: 'VERB, BASE FORM',
+    VBD: 'VERB, PAST TENSE',
+    VBG: 'VERB, GERUND OR PRESENT PARTICIPLE',
+    VBN: 'VERB, PAST PARTICIPLE',
+    VBP: 'VERB, NON-3RD PERSON SINGULAR PRESENT',
+    VBZ: 'VERB, 3RD PERSON SINGULAR PRESENT',
+    WDT: 'WH-DETERMINER',
+    WP: 'WH-PRONOUN',
+    WP$: 'POSSESSIVE WH-PRONOUN',
+    WRB: 'WH-ADVERB'
+}
+
 var dulceApp = angular.module('dulceApp', []);
 
 // See http://stackoverflow.com/questions/8302928/angularjs-with-django-conflicting-template-tags
@@ -6,11 +46,12 @@ dulceApp.config(function($interpolateProvider) {
 	$interpolateProvider.endSymbol(']]');
 });
 
-dulceApp.controller('AnalyseCtrl', function($scope) {
+dulceApp.controller('AnalyseCtrl', function($scope, $http) {
     $scope.raw = "";
     $scope.words = [];
-    $scope.reverse_alphabetical;
-    $scope.words_by_length;
+    $scope.reverse_alphabetical = null;
+    $scope.words_by_length = null;
+    $scope.pos_tagged = null;
 
     $scope.analyse = function(raw){
         if(!raw){return};
@@ -21,6 +62,19 @@ dulceApp.controller('AnalyseCtrl', function($scope) {
         }).sort();
         $scope.words = _.union(prepared);
 
+        $http.post('/api/1/pos', JSON.stringify({corpus: $scope.raw})).success(function(data){
+            console.log(data);
+            $scope.pos_tagged = [];
+            for(var tag in POSTAGS){
+                if(data[tag] != undefined){
+                    $scope.pos_tagged.push({
+                        definition: POSTAGS[tag],
+                        words: data[tag]
+                    });
+                };
+            };
+        });
+
         $scope.reverse_alphabetical = _.sortBy($scope.words, function(w){
             reversed = w.split("").reverse().join("");
             return reversed
@@ -29,6 +83,9 @@ dulceApp.controller('AnalyseCtrl', function($scope) {
         $scope.words_by_length = _.sortBy($scope.words, function(w){
             return w.length
         });
+
+
+
     }
 
     $scope.$watch('raw', $scope.analyse);
